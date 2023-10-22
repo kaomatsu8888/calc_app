1007dd43f1955ebc1dd97a394b763f9874a0ca0e
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-10-23 00:44:31 +0900

    calコントローラなしで完成

---
+++ resources/views/message/hello.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>     <!-- この行はHTML5の宣言 -->
+<html lang="ja">    <!-- この行は日本語のページであることを表す -->
+<head>           <!-- ここからHTMLのヘッダ情報を記述する -->
+    <meta charset="UTF-8">      <!-- 文字コードはUTF-8 -->
+    <title>VCApp</title>        <!-- ページのタイトル -->
+</head>     <!-- ここまでHTMLのヘッダ情報を記述する -->
+<body>      <!-- ここからHTMLの本文を記述する -->
+    <h1>Hello from View!!</h1>      <!-- 見出しを記述 -->
+</body>     <!-- ここまでHTMLの本文を記述する -->
+</html>     <!-- ここまでHTMLのページを記述する -->
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,58 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::<HTTPメソッド>('<アドレス>', クロージャ);
+Route::get('/route/hello', function () {
+    return '<h1>Hello from Route!</h1>';
+});
+
+// view/hello.blade.phpを表示
+Route::get('/view/hello', function () {
+    return view('message.hello');
+});
+
+// view/var.blade.phpを表示
+Route::get('/view/var', function () {
+    return view('message.var', ['variable' => 'Hello from web.php']);
+});
+
+// view/word.blade.phpを表示
+Route::get('/view/word/{msg}', function ($msg) {
+    return view('message.word', ['msg' => $msg]);
+});
+
+// view/word2.blade.phpを表示
+Route::get('/view/word/{name}/{msg}', function ($name, $msg) {
+    return view('message.word2', [
+        'name' => $name,
+        'msg' => $msg
+    ]);
+});
+
+// 四則演算 $num1, $operation, $num2を受け取り、view/calc.blade.phpを表示
+Route::get('calcs/{num1}/{operation}/{num2}', function ($num1, $operation, $num2) {
+    $result = 0; // 初期値は0
+    switch ($operation) {
+        case 'addition':    // 足し算
+            $result = $num1 + $num2;
+            break;
+        case 'substraction':    // 引き算
+            $result = $num1 - $num2;
+            break;
+        case 'multiplication':  // 掛け算
+            $result = $num1 * $num2;
+            break;
+        case 'division':    // 割り算
+            $result = $num1 / $num2;
+            break;
+    }
+    // view/calc.blade.phpを表示
+    return view('message.calc', [
+        'operation' => $operation,  // 演算子
+        'num1' => $num1,    // 左辺
+        'num2' => $num2,    // 右辺
+        'result' => $result // 結果
+    ]);
+});
